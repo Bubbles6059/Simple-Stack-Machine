4c5d30ea231462a9dcce083802e9c92c44140007
Bubblie123 <rohan.suresh712@gmail.com>
2024-09-25 23:59:44 -0400

    Revert "Did the -p tag and got it working decently but I have to check for any issues, going to start working on the outputting of the bof now"
    This reverts commit 5e9355d9651e3c80c45f94db85b48e688bf04342.

--- machine_main.c
+++ machine_main.c
@@ -1,35 +1,7 @@
 #include "bof.h"
 #include "instruction.h"
-#include "machine_types.h"
-#include "string.h"
-
-
-//Both used to check for the file name and the flag if you were to place it anywhere in the command line. 
-bool pFlagChecker(int arraySize, char** stringArray);
-char* findFileName(int arraySize, char** stringArray);
-
 int main(int argc, char** argv) {
-
-    BOFFILE boffile = bof_read_open(findFileName(argc, argv));
-    if(pFlagChecker(argc, argv)) {
-        printf("Found file creating header");
-        BOFHeader bofHeader = bof_read_header(boffile);
-    }
+    BOFFILE boffile = bof_read_open(argv[1]);
+    bof_read_header(boffile);
     return 0;
-}
-
-bool pFlagChecker(int arraySize, char** stringArray) {
-    for(int i = 0; i < arraySize; i++) {
-        if(strcmp(stringArray[i], "-p") == 0)
-            return true;
-    }
-    return false;
-}
-
-char* findFileName(int arraySize, char** stringArray) {
-    for(int i = 0; i < arraySize; i++) {
-        if(strcmp(stringArray[i], "./vm") != 0 && strcmp(stringArray[i], "-p") != 0)
-            return stringArray[i];
-    }
-    return "";
 }

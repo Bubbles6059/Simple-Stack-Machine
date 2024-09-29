8235592533e0dde02db9f0437d16dc4e5ee4f483
Alyssa Buckingham <lyssmbuckingham@gmail.com>
2024-09-29 05:13:31 -0400

    updated print format + references

--- machine_main.c
+++ machine_main.c
@@ -5,54 +5,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// memory size 2^16 words
+#define MEMORY_SIZE_IN_WORDS 32768
 #define LO 0
 #define HI 1
 
-// testing push by alyssa
-
-// memory size 2^16 words
-#define MEMORY_SIZE_IN_WORDS 32768
-
+// *** add comment ***
 static union mem_u {
 word_type words[MEMORY_SIZE_IN_WORDS];
 uword_type uwords[MEMORY_SIZE_IN_WORDS];
 bin_instr_t instrs[MEMORY_SIZE_IN_WORDS];
 } memory;
 
-//Both used to check for the file name and the flag if you were to place it anywhere in the command line. 
-bool pFlagChecker(int arraySize, char** stringArray);
+// processing arguments
 char* findFileName(int arraySize, char** stringArray);
+bool pFlagChecker(int arraySize, char** stringArray);
 
-int main(int argc, char** argv) 
+char* findFileName(int arraySize, char** stringArray) 
 {
-    //bin_instr_t* instructArray = malloc(sizeof(MEMORY_SIZE_IN_WORDS));
-    
-    BOFFILE boffile = bof_read_open(findFileName(argc, argv));
-    BOFHeader bofHeader = bof_read_header(boffile);
-    
-    if(pFlagChecker(argc, argv)) 
+    for(int i = 0; i < arraySize; i++) 
     {
-        
-        for(int i = 0; i < bofHeader.text_length; i++) 
-        {
-            memory.instrs[i] = instruction_read(boffile);
-        }
-
-        printf("Address Instruction\n");
-
-        for(int i = 0; i < bofHeader.text_length; i++) 
-        {
-            printf("\t%d: %s\n", i, instruction_assembly_form(i, memory.instrs[i]));
-        }
-        for(int i = 0; i < bofHeader.data_length; i++) {
-            printf("\t%d: %d", bofHeader.data_start_address + i, bof_read_word(boffile));
-        }
-        //bin_instr_t instruct = instruction_read(boffile);
-
-        // cut off after 2 consecutive zeros
-        
+        if(strcmp(stringArray[i], "./vm") != 0 && strcmp(stringArray[i], "-p") != 0)
+            return stringArray[i];
     }
-    return 0;
+    return "";
 }
 
 bool pFlagChecker(int arraySize, char** stringArray) 
@@ -65,12 +41,66 @@ bool pFlagChecker(int arraySize, char** stringArray)
     return false;
 }
 
-char* findFileName(int arraySize, char** stringArray) 
+int main(int argc, char** argv) 
 {
-    for(int i = 0; i < arraySize; i++) 
+    //bin_instr_t* instructArray = malloc(sizeof(MEMORY_SIZE_IN_WORDS));
+    
+    BOFFILE boffile = bof_read_open(findFileName(argc, argv));
+    BOFHeader bofHeader = bof_read_header(boffile);
+    
+    // loading program and printing
+    if(pFlagChecker(argc, argv)) 
     {
-        if(strcmp(stringArray[i], "./vm") != 0 && strcmp(stringArray[i], "-p") != 0)
-            return stringArray[i];
+        for(int i = 0; i < bofHeader.text_length; i++) 
+        {
+            memory.instrs[i] = instruction_read(boffile);
+        }
+
+        // *** add in \n after 57 char here ***
+
+        printf("Address Instruction\n");
+
+        for(int i = 0; i < bofHeader.text_length; i++) 
+        {
+            printf("%6d: %s\n", i, instruction_assembly_form(i, memory.instrs[i]));
+        }
+        for(int i = 0; i < bofHeader.data_length; i++) 
+        {
+            // *** cut off after 2 consecutive zeros here ***
+            printf("%8d: %d\t", bofHeader.data_start_address + i, bof_read_word(boffile));
+        }
+
+        //bin_instr_t instruct = instruction_read(boffile);
+
+        printf("        ...");
     }
-    return "";
+
+    // executing program and printing tracing output
+    else
+    {
+        // *** execute program here ***
+        /*
+            references
+            machine_execute_instr(bin_instr_t bi) in machine.c
+        */
+
+        // *** add in \n after 57 char here ***
+
+        for(int i = 0; i < bofHeader.text_length; i++) 
+        {
+            printf("        PC: %d\n", i);
+            // *** print GPR[$gp, $sp, $fp, $r3, $r4, $r5, $r6, $ra] here ***
+            /*  
+                references 
+                unparseReg(reg_num_type n)  asm_unparser.c
+                regname_get                 regname.c 
+
+            */
+
+            printf("\n");
+            printf("==> %6d: %s\n", i, instruction_assembly_form(i, memory.instrs[i]));
+        }
+    }
+
+    return 0;
 }
